23a4a92ecc83877e0da36c57ca2a478442c02fa9
Els <Emathivannan@spartaglobal.com>
2018-10-24 13:59:55 +0100

    Initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,29 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+apt_update "update" do
+  action :update
+end
+
+package "openjdk-8-jdk" do
+  action :install
+end
+
+apt_repository "add_jenkins" do
+  key "https://pkg.jenkins.io/debian-stable/jenkins.io.key"
+  uri "http://pkg.jenkins.io/debian-stable"
+  distribution "binary/"
+  action :add
+end
+
+apt_update "update" do
+  action :update
+end
+
+package "jenkins" do
+  action :install
+end
+
+service "jenkins" do
+  action [:start, :enable]
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -11,25 +11,32 @@ describe 'jenkins::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
-
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    it 'updates all sources' do
+      expect(chef_run).to update_apt_update('update')
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should install java' do
+      expect(chef_run).to install_package("openjdk-8-jdk")
+    end
+
+    it 'should install jenkin' do
+      expect(chef_run).to install_package("jenkins")
+    end
+
+    it 'should add jenkins to apt_repository' do
+      expect(chef_run).to add_apt_repository("add_jenkins")
+    end
+
+    it 'should check if jenkins starts' do
+      expect(chef_run).to start_service('jenkins')
+      expect(chef_run).to enable_service('jenkins')
     end
   end
 end
